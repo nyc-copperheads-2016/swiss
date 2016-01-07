edf2b9691b2abcc573978cc2dd45ec0629ed1b6d
Bruno Zatta <bzatta@gmail.com>
2016-01-07 17:16:48 -0500

    update bookmark model association add temp seed file

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ActiveRecord::Base
   has_many :user_bookmarks
   has_many :users, through: :user_bookmarks
 
-  validates_presence_of :url, :user_bookmarks
+  validates_presence_of :url
   validates_uniqueness_of :url
 end

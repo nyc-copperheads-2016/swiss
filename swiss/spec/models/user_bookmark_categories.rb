defc6c1f589cfb38f7e5a54cb1d8f6de8e1ce292
Chris Savage <chrislsavage@gmail.com>
2016-01-07 17:03:38 -0500

    Remove extra spec files under swiss/swiss

--- swiss/spec/models/user_bookmark_categories.rb
+++
@@ -1,4 +0,0 @@
-RSpec.describe User_Bookmark_Categories, type: :model do
-  it { should belong_to{:category}
-  it { should belong_to{:user_bookmark}}
-end
--- swiss/spec/models/user_bookmarks_spec.rb
+++
@@ -1,6 +0,0 @@
-RSpec.describe User_Bookmarks, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should belong_to(:bookmark)}
-  it { should belong_to{:user}}
-  it { should have_many{:user_bookmark_categories}}
-end

5b61daabc7d0b4badcd2914bc1478713b57fe0a1
Chris Savage <chrislsavage@gmail.com>
2016-01-08 12:39:28 -0500

    fix url validation

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,10 @@
 class Bookmark < ActiveRecord::Base
   has_many :user_bookmarks
   has_many :users, through: :user_bookmarks
+  validates :url, :presence => true, :uri => { :format => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix }
 
+  # validates :url, format: { with: URI.regexp }, if: Proc.new { |a| a.url.present? }
   validates_presence_of :url
   validates_uniqueness_of :url
+  # validates_format_of :url, :with => URI::regexp(%w(http https))
 end

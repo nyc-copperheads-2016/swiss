40f3c76b9117f4712b32c6bbb3797b148cefe7db
Bruno Zatta <bzatta@gmail.com>
2016-01-08 08:40:22 -0500

    Modify some syntax and association requirments for model specs, currently failing 2 tests for user and email uniqueness

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Bookmark, type: :model do
   it { should validate_presence_of(:url) }
   it { should validate_uniqueness_of(:url) }
-  it { should have_many(:users).through(:users_bookmarks) }
-  it { should have_many(:users_bookmarks) }
+  it { should have_many(:users).through(:user_bookmarks) }
+  it { should have_many(:user_bookmarks) }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 RSpec.describe Category, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_uniqueness_of(:name) }
-  it { should have_many(:users_bookmarks_categories)}
+  it { should have_many(:user_bookmark_categories)}
 end

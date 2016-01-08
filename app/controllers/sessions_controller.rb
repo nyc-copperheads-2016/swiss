4c88e26c616f550b9fbf5011bb5f2cb855d63318
Bruno Zatta <bzatta@gmail.com>
2016-01-07 19:48:33 -0500

    add sessions controller, review comments for param handling

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+# current params are dependant of how login form will be passing information, review prior to "done"
+class SessionsController < ApplicationController
+  skip_before_action :ensure_logged_in
+
+  def new
+  end
+
+  def create
+    user = User.find(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end

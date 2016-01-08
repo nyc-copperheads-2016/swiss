92626705410395b700f616437804419ef6bdf4b8
Bruno Zatta <bzatta@gmail.com>
2016-01-08 16:05:28 -0500

    Add trigram model and migration, add helper methods for html cleanup, add fuzzy search gem to gem file

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+
 class Bookmark < ActiveRecord::Base
   has_many :user_bookmarks
   has_many :users, through: :user_bookmarks
 
   validates_presence_of :url
   validates_uniqueness_of :url
+
+  fuzzily_searchable :content
+
+  def raw_content
+    Nokogiri::HTML(open("#{self.url}"))
+  end
+
+  def content
+    ApplicationHelper.clean(raw_content.text)
+  end
+
+  def content_changed?
+    url_changed?
+  end
 end
+

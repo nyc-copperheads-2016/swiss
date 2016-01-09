8af57982321c5b26ba5d760f02c3f86d97014898
Bruno Zatta <bzatta@gmail.com>
2016-01-09 17:15:45 -0500

    update fuzy search

--- app/controllers/user_bookmarks_controller.rb
+++ app/controllers/user_bookmarks_controller.rb
@@ -2,8 +2,9 @@ class UserBookmarksController < ApplicationController
   before_action :ensure_logged_in
 
   def index
-    if params[:search]
-      @search_results = current_user.bookmarks.find_by_fuzzy_content(params[:search])
+    if params[:search] != ""
+      results = Bookmark.find_by_fuzzy_content(params[:search], limit: 10)
+      @search_results = ApplicationHelper.search(results, params[:search])
     end
       @user = current_user
       @user_bookmarks = @user.user_bookmarks.all

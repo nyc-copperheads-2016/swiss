260b93fa0c3659118be7cbae77ce333d99755441
Chris Savage <chrislsavage@gmail.com>
2016-01-13 17:40:38 -0500

    ssl false in env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.force_ssl = false
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOWALL'}

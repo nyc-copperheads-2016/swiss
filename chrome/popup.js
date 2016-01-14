347db99bdea5eb97aa44992e87816b910572ddb5
Bruno Zatta <bzatta@gmail.com>
2016-01-14 11:33:19 -0500

    style chrome extension and replace icons

--- chrome/popup.js
+++ chrome/popup.js
@@ -6,8 +6,111 @@ var getUrlNamePath =  "https://warm-wildwood-8200.herokuapp.com/chrome";
 var postBookmarkPath = 'https://warm-wildwood-8200.herokuapp.com/chrome_create';
 var getLoginLinkPath = "https://warm-wildwood-8200.herokuapp.com/mlogin";
 
+// function loggedIn() {
+// $.get(getSessionPath, function(data) {
+//    if (data) {
+//      sessionStorage.loggedIn = true ;
+//      console.log("User is logged in");
+//      setFormVals().then(function(){postLink(data)});
+//     } else {
+//       console.log("hi");
+//       sessionStorage.clear();
+//       getLogin().then(function(){
+//         getHeader();
+//         logIn();
+//       });
+//     }
+//   });
+// }
+
+// $(document).ready(function(){
+// loggedIn();
+// })
+
+// function getHeader(){
+//   return $("#chrome_login a").on('click', function() {
+//     event.preventDefault();
+//     $.ajax({
+//       url: getLoginFormPath,
+//       method: 'GET'
+//     }).done(function(response){
+//       $('body').html(response);
+//     }).fail(function(error) {
+//       console.log("Error: " + error);
+//       });
+//   });
+// }
+
+// function getLogin(){
+// return $.get(getLoginLinkPath, function(form) {
+//         $("body").html(form);
+//       });
+// }
+
+//   function linkHome(){
+//   $("#chrome_app").on('click', 'a', function(event) {
+//     event.preventDefault();
+//     var newURL = getOpenTabPath;
+//     chrome.tabs.create({ url: newURL });
+//   });
+// }
+
+// function logIn () {
+//   $('html').on('submit','#user_form', function(){
+//     event.preventDefault();
+//     $.ajax({
+//       url: postLoginPath,
+//       method: "POST",
+//       data: $(event.target).serialize()
+//     }).then(function() {
+//       loggedIn();
+//       }).fail(function(error) {
+//         console.log("Error: " + error);
+//         });
+//   });
+// }
+
+// function setFormVals () {
+//   return $.get(getUrlNamePath, function(form){
+//     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+//       function(tabs){
+//         url = tabs[0].url;
+//         $('#url').val(url);
+//       });
+//       chrome.tabs.getSelected(null,function(tab) { // null defaults to current window
+//         title = tab.title;
+//         $('#name').val(title);
+//       });
+//     $("body").html(form);
+//     linkHome();
+//   });
+// }
+
+// function postLink (){$("#bookmark_form").on('submit', function() {
+//   event.preventDefault();
+//   $.ajax({
+//     url: postBookmarkPath,
+//     method: "POST",
+//     data: $(event.target).serialize()
+//   }).then(function(response) {
+//     console.log(response)
+//    $("body").html(response);
+//    linkHome();
+//     }).fail(function(error) {
+//       console.log("Error: " + error);
+//       });
+//   });
+// }
+
+
+
+
+
+
+
+
 function loggedIn() {
-$.get(getSessionPath, function(data) {
+$.get("http://localhost:3000/loggedin", function(data) {
    if (data) {
      sessionStorage.loggedIn = true ;
      console.log("User is logged in");
@@ -22,44 +125,11 @@ $.get(getSessionPath, function(data) {
     }
   });
 }
-
-$(document).ready(function(){
-loggedIn();
-})
-
-function getHeader(){
-  return $("#chrome_login a").on('click', function() {
-    event.preventDefault();
-    $.ajax({
-      url: getLoginFormPath,
-      method: 'GET'
-    }).done(function(response){
-      $('body').html(response);
-    }).fail(function(error) {
-      console.log("Error: " + error);
-      });
-  });
-}
-
-function getLogin(){
-return $.get(getLoginLinkPath, function(form) {
-        $("body").html(form);
-      });
-}
-
-  function linkHome(){
-  $("#chrome_app").on('click', 'a', function(event) {
-    event.preventDefault();
-    var newURL = getOpenTabPath;
-    chrome.tabs.create({ url: newURL });
-  });  
-}
-
 function logIn () {
   $('html').on('submit','#user_form', function(){
     event.preventDefault();
     $.ajax({
-      url: postLoginPath,
+      url: 'http://localhost:3000/login',
       method: "POST",
       data: $(event.target).serialize()
     }).then(function() {
@@ -69,9 +139,31 @@ function logIn () {
         });
   });
 }
-
+$(document).ready(function(){
+loggedIn();
+})
+function getHeader(){
+  return $("#chrome_login a").on('click', function() {
+    event.preventDefault();
+    $.ajax({
+      url:'http://localhost:3000/chrome_new',
+      method: 'GET'
+    }).done(function(response){
+      $('#body').html(response);
+    }).fail(function(error) {
+      console.log("Error: " + error);
+      });
+    });
+  }
+function linkHome(){
+  $("#chrome_app").on('click', 'a', function(event) {
+    event.preventDefault();
+    var newURL = "http://localhost:3000/user_bookmarks";
+    chrome.tabs.create({ url: newURL });
+  });
+}
 function setFormVals () {
-  return $.get(getUrlNamePath, function(form){
+  return $.get("http://localhost:3000/chrome", function(form){
     chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
       function(tabs){
         url = tabs[0].url;
@@ -81,24 +173,27 @@ function setFormVals () {
         title = tab.title;
         $('#name').val(title);
       });
-    $("body").html(form);
+    $("#body").html(form);
     linkHome();
   });
 }
-
 function postLink (){$("#bookmark_form").on('submit', function() {
   event.preventDefault();
   $.ajax({
-    url: postBookmarkPath,
+    url: 'http://localhost:3000/chrome_create',
     method: "POST",
     data: $(event.target).serialize()
   }).then(function(response) {
     console.log(response)
-   $("body").html(response);
+   $("#body").html(response);
    linkHome();
     }).fail(function(error) {
       console.log("Error: " + error);
       });
   });
 }
-
+function getLogin(){
+return $.get("http://localhost:3000/mlogin", function(form) {
+        $("#body").html(form);
+      });
+}

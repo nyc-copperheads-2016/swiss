593154751295c1f34c2d69dae15cddca457d0de0
Chris Savage <chrislsavage@gmail.com>
2016-01-14 09:57:55 -0500

    rspec green

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
   it { should have_many(:user_bookmark_categories)}
+
+  it "is invalid without a unique, case-sensitive name" do
+    bookmark = Category.create(name: "jon")
+    bookmark2 = Category.create(name: "Jon")
+    expect(bookmark2).to be_valid
+  end
 end
